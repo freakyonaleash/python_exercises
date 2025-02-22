9ac4dd1a3394ef717454d1b4ef3a26fa03439d2b
freakyonaleash <freakzonaleash@gmail.com>
2025-02-22 23:38:07 +0200

    Updated the dice_roller logic

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,15 @@
 from random import randint
 
 sides = int(input("How many sides is the die? "))
+
+while sides <=0 or sides >=21 or not sides.isdigit():
+    sides = int(input("Type in the number from 1 to 20: "))
+
 quantity = int(input("How many dice are you throwing? "))
 
-while (sides or quantity) <=0 or (sides or quantity) >=21:
-    sides = input("Type in the number from 1 to 20")
-    quantity = input("Type in the number from 1 to 20")
-    
-    
+while quantity <=0 or quantity >=21:
+    quantity = int(input("Type in the number from 1 to 20: "))
+   
 output = "|"
 for run in range (0, quantity):
     roll = randint (1, sides)
