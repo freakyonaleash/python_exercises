e06f2d23619533d5b641eb253e1c396f05a9e4aa
freakyonaleash <freakzonaleash@gmail.com>
2025-03-04 23:25:11 +0200

    Updated practice file with nex examples

--- practice.py
+++ practice.py
@@ -64,17 +64,70 @@ from random import randint
 #     for inner  in range (10,0,-1):
 #         print ("\t", "inner")
 
-num_dice = int(input("How many dice?"))
-num_sides = int(input("How many sides?"))
+# def laugh():
+#     print ("HA!"*20)
 
-while True:
-    output = "|"
-    for die in range (num_dice):
-        roll = (randint(1, num_sides))
-        output += f"{roll} |"
-    print (output)
-    reply = input ("Roll again? ('q' to quit)")
-    if reply == "q":
-         break
+# laugh()
 
+# def laugh(strenth):
+#     print ("HA!"*strenth)
 
+# laugh (2)
+# laugh (8)
+# laugh (20)
+
+# word = "Sassy"
+
+# word = word.replace("s", "$")
+
+# print (word)
+
+# def divide (x,y):
+#     print (x/y)
+
+# divide (20,5)
+
+# def divide (x,y):
+#     return x/y
+
+# res = divide (30,5)
+# print (res)
+
+# def is_even(number):
+#     if number % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# result = is_even(3)
+# print (result)
+
+# def is_even(number):
+#     if number % 2 == 0:
+#         return True
+#     return False
+
+# result = is_even(3)
+# print (result)
+
+# def is_even(number):
+#     return number % 2 == 0
+
+# result = is_even(3)
+# print (result)
+# def slugify (message):
+#     return message.strip().lower().replace(" ", "-")
+
+# result = slugify (" Hey HEY hEYYY ")
+# print (result)
+
+def vowel_counter (message):
+    vowel = 0
+    for itr in message:
+        if itr in ("aeiou"):
+            vowel +=1
+    print(vowel)
+    return vowel
+
+result = vowel_counter ("Helloooo")
+print (result)
